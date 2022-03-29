9f60087f3b1e71dc9bbda16c6077bab2c2e9e8ed
dhairya <joshidhairaya2002@gmail.com>
2022-03-29 22:31:27 +0530

    minor changes

--- lib/screens/services/database_services.dart
+++ lib/screens/services/database_services.dart
@@ -1,24 +1,20 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_app/screens/models/ngo_model.dart';
 
-
-class DatabaseService{
-
-  Future<List<NGO>> getNgos() async{
-    List<NGO> ngolist=  [];
+class DatabaseService {
+  Future<List<NGO>> getNgos() async {
+    List<NGO> ngolist = [];
     QuerySnapshot querySnapshot =
         await FirebaseFirestore.instance.collection("NGOs").get();
 
-    for(int i=0;i<querySnapshot.docs.length;i++)
-    {
-      String name= querySnapshot.docs[i]['name'];
-      String desc= querySnapshot.docs[i]['desc'];
-      String pfp= querySnapshot.docs[i]['pfp'];
-      String location= querySnapshot.docs[i]['location'];
-      ngolist.add(NGO(name,desc,pfp,location));
+    for (int i = 0; i < querySnapshot.docs.length; i++) {
+      String name = querySnapshot.docs[i]['name'];
+      String desc = querySnapshot.docs[i]['desc'];
+      String pfp = querySnapshot.docs[i]['pfp'];
+      String location = querySnapshot.docs[i]['location'];
+      ngolist.add(NGO(name, desc, pfp, location));
     }
 
     return ngolist;
   }
-
 }
