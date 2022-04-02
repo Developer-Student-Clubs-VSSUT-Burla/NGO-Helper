b6d0d719139ded8544e397c0d9d4ccec3a8e9803
Suraj Mahapatra <surajjiku@gmail.com>
2022-04-02 16:42:52 +0530

    firebase

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,10 +1,13 @@
+
+
 // ignore_for_file: deprecated_member_use
 
+import 'package:email_validator/email_validator.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-
+import 'package:ngo_helper/screens/reset.dart';
 import '../main.dart';
 
 class LogIn extends StatefulWidget {
@@ -32,79 +35,118 @@ class _LogInState extends State<LogIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        // appBar: AppBar(
-        //   title:Text("Login"),
-        //   backgroundColor: Colors.teal,
-        // ),
         body: ListView(
-      padding: const EdgeInsets.fromLTRB(0.0, 150.0, 0.0, 0.0),
-      children: <Widget>[
-        const Text("Login",
-            style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 40.0,
-                color: Colors.teal),
-            textAlign: TextAlign.center),
-        Container(
-            padding: const EdgeInsets.all(30.0),
-            child: TextField(
-                controller: emailController,
-                cursorColor: Colors.white,
-                textInputAction: TextInputAction.next,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20.0)),
-                  hintText: 'Email',
-                ))),
-        Container(
-            padding: const EdgeInsets.fromLTRB(30.0, 0.0, 30.0, 0.0),
-            child: TextField(
-              controller: passwordController,
-              textInputAction: TextInputAction.done,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20.0)),
-                hintText: 'Password',
-              ),
-              obscureText: true,
-            )),
-        Container(
-          height: 50.0,
-          margin: const EdgeInsets.fromLTRB(30.0, 20.0, 30.0, 0.0),
-          child: RaisedButton(
-            onPressed: signin,
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-            child: const Text(
-              "Login",
-              style: TextStyle(color: Colors.white, fontSize: 20.0),
-              textAlign: TextAlign.center,
-            ),
-            color: Colors.teal,
-          ),
-        ),
-        const SizedBox(height: 60.0),
-        Center(
-          child: RichText(
-            text: TextSpan(
-                style: const TextStyle(color: Colors.black45, fontSize: 18.0),
-                text: 'No account? ',
-                children: [
-                  TextSpan(
-                    recognizer: TapGestureRecognizer()
-                      ..onTap = widget.onClickedSignedUp,
-                    text: 'Sign Up',
-                    style: const TextStyle(
-                      decoration: TextDecoration.underline,
-                      color: Colors.teal,
-                      fontSize: 17.0,
+
+          padding: const EdgeInsets.fromLTRB(0.0, 150.0, 0.0, 0.0),
+          children: <Widget>[
+            const Text("Login",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 40.0,
+                    color: Colors.teal),
+                textAlign: TextAlign.center),
+            SizedBox(height: 40),
+            Container(
+                padding: const EdgeInsets.fromLTRB(30.0, 0.0, 30.0, 20.0),
+                child: TextFormField(
+                  controller: emailController,
+                  cursorColor: Colors.white,
+                  textInputAction: TextInputAction.next,
+                  decoration: InputDecoration(
+                    filled: true,
+                    labelText: 'Email',
+                    fillColor: Colors.grey[280],
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.teal,width: 2.0),
+                      borderRadius: BorderRadius.circular(20.0),
                     ),
                   ),
-                ]),
-          ),
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  validator: (email)=>
+                  email != null && EmailValidator.validate(email)
+                      ? null
+                      : 'Enter a valid email',
+                )
+            ),
+            Container(
+                padding: const EdgeInsets.fromLTRB(30.0, 0.0, 30.0, 0.0),
+                child: TextFormField(
+                  controller: passwordController,
+                  textInputAction: TextInputAction.done,
+                  decoration: InputDecoration(
+                    filled: true,
+                    labelText: 'Password',
+                    fillColor: Colors.grey[280],
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.teal,width: 2.0),
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                  ),
+                  obscureText: true,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  validator: (value)=>
+                  value != null && value.length<6
+                      ? 'Enter minimum 6 characters'
+                      : null,
+                )
+            ),
+            SizedBox(height: 40),
+            Container(
+              height: 50.0,
+              margin: const EdgeInsets.fromLTRB(30.0, 20.0, 30.0, 0.0),
+              child: RaisedButton(
+                onPressed: signin,
+                shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                child: const Text(
+                  "Login",
+                  style: TextStyle(color: Colors.white, fontSize: 20.0),
+                  textAlign: TextAlign.center,
+                ),
+                color: Colors.teal,
+              ),
+            ),
+            const SizedBox(height: 30.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                TextButton(
+                    onPressed: () => Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => ResetB())),
+                    child:Text('Forgot Password',
+                      style: TextStyle(
+                          color: Colors.teal,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16.0
+                      ),
+                    )
+                )
+              ],
+            ),
+            const SizedBox(height: 70.0),
+            Center(
+              child: RichText(
+                text: TextSpan(
+                    style: const TextStyle(color: Colors.black45, fontSize: 18.0),
+                    text: 'No account ? ',
+                    children: [
+                      TextSpan(
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = widget.onClickedSignedUp,
+                        text: 'Sign Up',
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          decoration: TextDecoration.underline,
+                          color: Colors.teal,
+                          fontSize: 17.0,
+                        ),
+                      ),
+                    ]),
+              ),
+            )
+          ],
         )
-      ],
-    ));
+    );
   }
 
   Future signin() async {
--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -1,3 +1,4 @@
+import 'package:email_validator/email_validator.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
@@ -25,93 +26,128 @@ class _SignUpState extends State<SignUp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        // appBar: AppBar(
-        //   title:Text("Signup"),
-        //   backgroundColor: Colors.teal,
-        // ),
-        body: ListView(
-            padding: const EdgeInsets.fromLTRB(0.0, 150.0, 0.0, 0.0),
-            children: <Widget>[
-          const Text(
-            "Signup",
-            style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 40.0,
-                color: Colors.teal),
-            textAlign: TextAlign.center,
-          ),
-          Container(
-            padding: const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 0.0),
-            child: TextField(
-                controller: displayNameController,
-                cursorColor: Colors.white,
-                textInputAction: TextInputAction.next,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20.0)),
-                  hintText: 'Your Name',
-                )),
-          ),
-          Container(
-            padding: const EdgeInsets.all(30.0),
-            child: TextField(
-                controller: emailController,
-                cursorColor: Colors.white,
-                textInputAction: TextInputAction.next,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20.0)),
-                  hintText: 'Your Email',
-                )),
-          ),
-          Container(
-            padding: const EdgeInsets.fromLTRB(30.0, 0.0, 30.0, 0.0),
-            child: TextField(
-                controller: passwordController,
-                textInputAction: TextInputAction.done,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20.0)),
-                  hintText: 'Enter Password',
-                ),
-                obscureText: true),
-          ),
-          Container(
-            height: 50.0,
-            margin: const EdgeInsets.fromLTRB(30.0, 20.0, 30.0, 0.0),
-            child: ElevatedButton(
-              onPressed: signup,
-              style: ElevatedButton.styleFrom(
-                  primary: Colors.teal,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20))),
-              child: const Text("Continue",
-                  style: TextStyle(color: Colors.white, fontSize: 20.0)),
-            ),
-          ),
-          const SizedBox(height: 60.0),
-          Center(
-            child: RichText(
-              text: TextSpan(
-                  style: const TextStyle(
-                    color: Colors.black45,
-                    fontSize: 18.0,
-                  ),
-                  text: 'Already have an account? ',
-                  children: [
-                    TextSpan(
-                      recognizer: TapGestureRecognizer()
-                        ..onTap = widget.onClickedSignIn,
-                      text: 'Log In',
-                      style: const TextStyle(
-                          //decoration: TextDecoration.underline,
-                          color: Colors.teal,
-                          fontSize: 17.0),
+        body: Center(
+          child: ListView(
+              padding: const EdgeInsets.fromLTRB(0.0, 130.0, 0.0, 0.0),
+              children: <Widget>[
+                Container(
+                  child: const Text(
+                    "Sign Up",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 40.0,
+                        color: Colors.teal
                     ),
-                  ]),
-            ),
-          )
-        ]));
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                SizedBox(height: 40),
+                Container(
+                  padding: const EdgeInsets.fromLTRB(30.0, 0.0, 30.0, 20.0),
+                  child: TextField(
+                      controller: displayNameController,
+                      cursorColor: Colors.white,
+                      textInputAction: TextInputAction.next,
+                      decoration: InputDecoration(
+                        filled: true,
+                        labelText: 'Your Name',
+                        fillColor: Colors.grey[280],
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.teal,width: 2.0),
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                      )
+                  ),
+                ),
+
+                Container(
+                  padding: const EdgeInsets.fromLTRB(30.0, 0.0, 30.0, 20.0),
+                  child: TextFormField(
+                    controller: emailController,
+                    cursorColor: Colors.white,
+                    textInputAction: TextInputAction.next,
+                    decoration: InputDecoration(
+                      filled: true,
+                      labelText: 'Email',
+                      fillColor: Colors.grey[280],
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.teal,width: 2.0),
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                    ),
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    validator: (email)=>
+                    email != null && EmailValidator.validate(email)
+                        ? null
+                        : 'Enter a valid email',
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.fromLTRB(30.0, 0.0, 30.0, 0.0),
+                  child: TextFormField(
+                    controller: passwordController,
+                    textInputAction: TextInputAction.done,
+                    decoration: InputDecoration(
+                      filled: true,
+                      labelText: 'Password',
+                      fillColor: Colors.grey[280],
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.teal,width: 2.0),
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                    ),
+                    obscureText: true,
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    validator: (value)=>
+                    value != null && value.length<6
+                        ? 'Enter minimum 6 characters'
+                        : null,
+                  ),
+                ),
+                SizedBox(height: 40),
+                Container(
+                  height: 50.0,
+                  margin: const EdgeInsets.fromLTRB(30.0, 20.0, 30.0, 0.0),
+                  child: ElevatedButton(
+                    onPressed: signup,
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.teal,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        )),
+
+                    child: const Text("SignUp",
+                        style: TextStyle(color: Colors.white, fontSize: 20.0)),
+                  ),
+                ),
+                const SizedBox(height: 30.0),
+                Center(
+                  child: RichText(
+                    text: TextSpan(
+                        style: const TextStyle(
+                          color: Colors.black45,
+                          fontSize: 18.0,
+                        ),
+                        text: 'Already have an account ? ',
+                        children: [
+                          TextSpan(
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = widget.onClickedSignIn,
+                            text: 'Log In',
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.teal,
+                                decoration: TextDecoration.underline,
+                                fontSize: 17.0),
+                          ),
+                        ]),
+                  ),
+                )
+              ]
+          ),
+        )
+
+    );
   }
 
   Future signup() async {
@@ -129,9 +165,7 @@ class _SignUpState extends State<SignUp> {
       // print(user);
       user.updateDisplayName(displayNameController.text.trim());
     } on FirebaseAuthException catch (e) {
-      if (kDebugMode) {
-        print(e);
-      }
+      print(e);
     }
 
     navigatorKey.currentState!.popUntil((route) => route.isFirst);
